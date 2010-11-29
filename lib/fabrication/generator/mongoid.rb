0a1d8b002602c54e35583b239e229efbf1620175
Micah Winkelspecht <winkelspecht@gmail.com>
2010-11-28 16:10:00 -0800

    changing from save to save! for Mongoid

--- lib/fabrication/generator/mongoid.rb
+++ lib/fabrication/generator/mongoid.rb
@@ -3,5 +3,10 @@ class Fabrication::Generator::Mongoid < Fabrication::Generator::Base
   def self.supports?(klass)
     defined?(Mongoid) && klass.ancestors.include?(Mongoid::Document)
   end
-
+  
+  private
+  
+  def after_generation(options)
+    instance.save! if options[:save] && instance.respond_to?(:save!)
+  end
 end
